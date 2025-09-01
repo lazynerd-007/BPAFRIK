ec0bb0a1c1190c3370af79d62b8a23c44654fd10
Lazynerd-007 <babsodunewu@lazynerdstudios.com>
2025-09-01 15:30:55 +0000

    upd2

---
+++ types/merchant.d.ts
@@ -0,0 +1,15 @@
+declare module '@/components/merchant/view-merchants' {
+  export function ViewMerchants(): JSX.Element;
+}
+
+declare module '@/components/merchant/view-sub-merchants' {
+  export function ViewSubMerchants(): JSX.Element;
+}
+
+declare module '@/components/merchant/create-merchant-form' {
+  export function CreateMerchant(): JSX.Element;
+}
+
+declare module '@/components/merchant/create-sub-merchant-form' {
+  export function CreateSubMerchant(): JSX.Element;
+} 
